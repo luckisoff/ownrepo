3b2767fc66f367e4870fad1d94bda7179f4f14c2
SRBN Media Nepal <srbnmedianepal@gmail.com>
2019-11-01 15:38:08 +0545

    updated

--- app/Http/Controllers/QuestionLevelController.php
+++ app/Http/Controllers/QuestionLevelController.php
@@ -66,9 +66,9 @@ class QuestionLevelController extends CommonController {
 		// }])->orderby('updated_at','asc')->limit(1)->get()->pluck('question')->random();
 		//$questions=Question::with(['setquestion','questionType'])->orderBy('created_at','desc')->get()->take(15);
 		
-		return Setquestion::with(['question'=>function($q){
+		return Setquestion::where('question_type_id',$country)->whereHas('question')->with(['question'=>function($q){
 			$q->with('options');
-		}])->whereHas('question')->where('question_type_id',$country)->random();
+		}])->orderBy(rand())->get();
 
 		// $return_data = $this->format_multi_lang($setquestion);
 
