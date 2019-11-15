7ef654d03f66c7e9eef6ff1718280c953e9f6e19
SRBN Media Nepal <srbnmedianepal@gmail.com>
2019-11-15 11:33:34 +0545

    update

--- app/Http/Controllers/Api/QuestionSetController.php
+++ app/Http/Controllers/Api/QuestionSetController.php
@@ -22,7 +22,7 @@ class QuestionSetController extends CommonController {
 		$questions = Question::with(['conversions', 'options', 'options.conversions', 'question_set'])
 		                     ->whereHas('question_set', function($query) {
 			                     $query->where('start_time', '>=', today())
-				                     ->where('end_time', '<=', now());
+				                     //->where('end_time', '<=', now());
 		                     })
 							 ->get();
 							 
